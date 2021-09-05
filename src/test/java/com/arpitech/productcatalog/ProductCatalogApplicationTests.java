7f479f4590b45c560fae7f10a8ed856296c588c7
sandroteste72 <sandro.teste72@gmail.com>
2021-09-05 02:48:40 -0300

    First commit

---
+++ src/test/java/com/arpitech/productcatalog/ProductCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpitech.productcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
